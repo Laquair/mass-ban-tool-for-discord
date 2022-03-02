bae7a2eaf8165bacb67bf5e4d498b2b1761393c2
Laquair <97777202+Laquair@users.noreply.github.com>
2022-03-02 23:29:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,15 +15,18 @@ server = int(input('guild id  :'))
 os.system("clear")
 owo = int(input('type 1 to ban   :'))
 guild = bot.get_guild(server)
-if owo == 1:
-   def ban(guild, member):
-    json = {
+
+def ban(guild, member):
+        try:
+            json = {
                 'delete_message_days': '7',
                 'reason': 'Laquairc op'
             }
             r = requests.put(f'https://discord.com/api/v10/guilds/{guild}/bans/{member}', headers=headers, json=json)
             if r.status_code == 200 or r.status_code == 201 or r.status_code == 204:
                 print(f"killed {member.name}")
+            
+            
 
 while True:
    threading.Thread(target=ban).start()
